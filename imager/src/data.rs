bfed983f36bf2256f581c7dd39c18bdca427993b
Igor Demin <vpldzzwaac2i@mail.ru>
2023-06-13 23:30:56 +0400

    chande mod.rs'

--- imager/src/data.rs
+++ imager/src/data.rs
@@ -48,8 +48,7 @@ impl FromStr for OutputFormat {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
-            "jpeg" => Ok(OutputFormat::Jpeg),
-            "jpg" => Ok(OutputFormat::Jpeg),
+            "jpeg" | "jpg" => Ok(OutputFormat::Jpeg),
             "png" => Ok(OutputFormat::Png),
             "webp" => Ok(OutputFormat::Webp),
             _ => Err(format!("Unknown or unsupported output format {}", s)),
